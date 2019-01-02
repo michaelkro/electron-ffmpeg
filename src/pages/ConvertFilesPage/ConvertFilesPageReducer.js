d61edfd6d73ae57696e0f39437b813524839259d
Mike Ro <mikerodev@gmail.com>
2019-01-01 17:34:18 -0800

    Add eslint

--- src/pages/ConvertFilesPage/ConvertFilesPageReducer.js
+++ src/pages/ConvertFilesPage/ConvertFilesPageReducer.js
@@ -1,10 +1,8 @@
-import { ADD_FILES } from './ConvertFilesPageConstants';
-
-const initialState = {};
+const initialState = {}
 
 export default (state = initialState, action) => {
   switch (action.type) {
     default:
-      return state;
+      return state
   }
-};
+}
